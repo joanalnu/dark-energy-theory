704425eeb80cff4674d2dc1fc7a2448acfea3d57
joanalnu <joanalnu@outlook.com>
2025-12-12 23:57:42 +0100

    move friedmann/LCDM files to code

--- to-sort-out/friedmann/parameters.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-H0 = 70  # or whatever value in km/s/Mpc
-z = np.linspace(0.01, 10, 100)  # a range of redshifts
-OmegaM = 0.3
-OmegaDE = 0.7
-OmegaR = 0.0  # if ignoring radiation, else something like ~1e-4
